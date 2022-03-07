80a6438200d9940a7c4fa1de4e68bca84c90dc22
Ahmed Elwerdany <infoalwerdany@gmail.com>
2022-03-07 18:02:54 +0200

    refactor: add default options in constants.ts file

---
+++ ts/src/constants.ts
@@ -0,0 +1,9 @@
+export interface OptionsI {
+    invokeAfterName: string;
+    invokeBeforeName: string;
+}
+
+export const DEFAULT_OPTIONS: OptionsI = {
+    invokeAfterName: 'invokeAfter',
+    invokeBeforeName: 'invokeBefore',
+}
